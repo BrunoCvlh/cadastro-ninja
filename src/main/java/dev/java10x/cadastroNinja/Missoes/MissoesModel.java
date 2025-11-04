94381bd9f7e1e10f346cd9eb1c847fc94dbe4d32
Bruno <BrunoCvlh>
2025-11-04 17:27:12 -0300

    fix: incluído a annotation @JsonIgnore para resulver o problema de loop no método listarNinjas

--- src/main/java/dev/java10x/cadastroNinja/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastroNinja/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.cadastroNinja.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.cadastroNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -24,6 +25,7 @@ public class MissoesModel {
     private String dificuldade;
 
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 
 }
